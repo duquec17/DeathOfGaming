744332e4a2fd738f09f4c9aa0673e2a757c46ea6
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2025-11-24 12:02:57 -0500

    Collectables
    Setup the basic for working collectable
    
    Specific to this game:
    - Ammo: For weapon in combat level
    - Defense: Camp resource
    - Batteries/Energy: Camp resource
    - Food: Camp resource
    - Rec: Camp resource & main score

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    /* - Player wwalks into collectable
+       - addd collectable to player, counter, or inventory
+       - Delete collectable from the screen
+    */
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+
+        if (player != null)
+        {
+            player.Ammo++;
+            Destroy(this.gameObject);
+        }
+    }
+}
