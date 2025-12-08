527eba85dab75246b2b6fd5bd91bd0000d367388
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2025-12-08 11:55:32 -0500

    Basic Interactable Tile System
    Allows to create interactable tiles on over lapping tile map. Possible usages:
    - Fast travel map locations
    - Scene transitions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,21 @@ public class Player : MonoBehaviour
         inventory = new Inventory(21);
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Vector3Int position = new Vector3Int((int)transform.position.x, 
+                (int)transform.position.y, 0);
+
+            if (GameManager.instance.tileManager.isInteractable(position))
+            {
+                Debug.Log("Tile is interactable");
+                GameManager.instance.tileManager.SetInteracted(position);
+            }
+        }
+    }
+
     public void DropItem(Collectable item)
     {
         Vector2 spawnLocation = transform.position;
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class TileManager : MonoBehaviour
+{
+    [SerializeField] private Tilemap interactableMap;
+
+    [SerializeField] private Tile hiddenInteractableTile;
+    [SerializeField] private Tile interactedTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var position in interactableMap.cellBounds.allPositionsWithin)
+        {
+            TileBase tile = interactableMap.GetTile(position);
+
+            if(tile != null && tile.name == "Interactable_Visible")
+            {
+                interactableMap.SetTile(position, hiddenInteractableTile);
+            }
+        }
+    }
+
+    public bool isInteractable(Vector3Int position)
+    {
+        TileBase tile = interactableMap.GetTile(position);
+
+        if(tile != null)
+        {
+            if(tile.name == "Interactable")
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void SetInteracted(Vector3Int position)
+    {
+        interactableMap.SetTile(position, interactedTile);
+    }
+}
