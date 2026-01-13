efd4e0dabb296610913e08e6dfd45132a4735758
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2026-01-13 12:58:13 -0500

    minor size and box changes

--- Assets/Scripts/TownScripts/BuildInfo.cs
+++ Assets/Scripts/TownScripts/BuildInfo.cs
@@ -7,16 +7,18 @@ public class BuildInfo : MonoBehaviour
 {
     public string message;
 
+    // When building clicked on will display information
     private void OnMouseDown()
     {
         BuildInfoManager._instance.SetAndShowBuildInfo(message);
-        GetComponent<BoxCollider2D>().enabled = false;
+        //GetComponent<BoxCollider2D>().enabled = false;
     }
 
+    // Will hide info when clicked outside of box
     public void DeleteInfo()
     {
         BuildInfoManager._instance.HideBuildInfo();
-        GetComponent<BoxCollider2D>().enabled = true;
+        //GetComponent<BoxCollider2D>().enabled = true;
     }
 
 }
