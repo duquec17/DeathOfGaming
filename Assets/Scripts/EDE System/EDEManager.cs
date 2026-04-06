0b1699a52510a0d389b3fe9dd33160442816dab5
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2026-04-06 13:09:43 -0400

    Persistent Data (cont.5)
    - Added simple main menu to start and quit game.
    - Adjusted EDE choice system so buttons update to give different effect based on event type
    - Next will have to create proper win/loss condition

--- Assets/Scripts/EDE System/EDEManager.cs
+++ Assets/Scripts/EDE System/EDEManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 [Serializable]
@@ -29,6 +30,8 @@ public class EDEManager : MonoBehaviour
     public float eventResult;
 
     public GameObject dialogueContainer;
+    public Button choiceOne, choiceTwo, choiceThree;
+
     public TMP_Text event_NPC_Name;
     public TMP_Text event_NPC_Dialogue;
     public TMP_Text choice_1;
@@ -92,6 +95,7 @@ public class EDEManager : MonoBehaviour
         {
             case WheelEvent.Nothing:
                 Debug.Log("Nothing happened. Next day.");
+                dialogueContainer.SetActive(false);
                 break;
 
             case WheelEvent.EventA:
@@ -115,36 +119,48 @@ public class EDEManager : MonoBehaviour
     {
         // spawn enemies, show dialogue, etc
         dialogueContainer.SetActive(true);
-        choice_1.text = "A1 Choice - Gain 25 Food";
+        choice_1.text = "A1 Choice - Gain 30 Food";
+        choiceOne.onClick.AddListener(EventAddFoodResource);
         choice_2.text = "A2 Choice";
-        choice_3.text = "A3 Choice";
+        choice_3.text = "A3 Choice - Skip";
+
     }
 
     void TriggerEventB()
     {
         // different event logic
         dialogueContainer.SetActive(true);
-        // spawn enemies, show dialogue, etc
-        dialogueContainer.SetActive(true);
-        choice_1.text = "B1 Choice - Gain 25 Food";
+        choice_1.text = "B1 Choice - Gain 30 Energy";
+        choiceOne.onClick.AddListener(EventAddEnergyResource);
         choice_2.text = "B2 Choice";
-        choice_3.text = "B3 Choice";
+        choice_3.text = "B3 Choice  - Skip";
     }
 
     void TriggerEventC()
     {
         // another event
         dialogueContainer.SetActive(true);
-        // spawn enemies, show dialogue, etc
-        dialogueContainer.SetActive(true);
-        choice_1.text = "C1 Choice - Gain 25 Food";
+        choice_1.text = "C1 Choice - Gain 30 Defense";
+        choiceOne.onClick.AddListener(EventAddDefenseResource);
         choice_2.text = "C2 Choice";
-        choice_3.text = "C3 Choice";
+        choice_3.text = "C3 Choice  - Skip";
     }
 
     public void EventAddFoodResource()
     {
         TownBehaviour.total_FOOD += 30;
         Debug.Log("Current Food Total: " + TownBehaviour.total_FOOD);
+    }
+
+    public void EventAddEnergyResource()
+    {
+        TownBehaviour.total_ENG += 30;
+        Debug.Log("Current Energy Total: " + TownBehaviour.total_ENG);
+    }
+
+    public void EventAddDefenseResource()
+    {
+        TownBehaviour.total_DEF += 30;
+        Debug.Log("Current Defense Total: " + TownBehaviour.total_DEF);
     }
 }
