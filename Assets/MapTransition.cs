8ef8b1ed1d2f50c22eeaf81974b7052560032c54
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2026-06-09 16:46:15 -0400

    Basic Manual Map Done
    - Contains map change icons
    - Player icon viewable on map
    -Background art/image for map
    - Works with save system
    Note: Appears as best option for a more well structure level layout

--- Assets/MapTransition.cs
+++ Assets/MapTransition.cs
@@ -23,6 +23,8 @@ public class MapTransition : MonoBehaviour
         {
             confiner.m_BoundingShape2D = mapBoundary;
             UpdatePlayerPosition(collision.gameObject);
+
+            MapController_Manual.Instance?.HighlightArea(mapBoundary.name);
         }
     }
 
